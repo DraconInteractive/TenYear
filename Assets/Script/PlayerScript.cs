59b89cbdbf1e93e121c413fcb40b691146888b3c
Peter Carey <rarceth@gmail.com>
2015-11-26 01:50:09 +0800

    Initial Player Movement

---
+++ Assets/Script/PlayerScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	public Rigidbody thisRigidbody;
+	public Transform thisTransform;
+	public float speed, health;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PlayerMovement(){
+		if (Input.GetMouseButtonDown(0)){
+
+		}
+		if (Input.GetMouseButtonDown(1)){
+
+		}
+
+
+	}
+}
