f1a0df1ebd9c5eb202d8ebd65b23976830738d51
Peter Carey <rarceth@gmail.com>
2015-11-26 01:58:52 +0800

    All Inputs Initiated

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -5,6 +5,7 @@ public class PlayerScript : MonoBehaviour {
 	public float speed, health, jumpForce;
 	public Rigidbody thisRigidbody;
 	public Transform thisTransform;
+	public bool showConsole;
 
 	// Use this for initialization
 	void Start () {
@@ -19,10 +20,10 @@ public class PlayerScript : MonoBehaviour {
 	public void PlayerInput(){
 
 		if (Input.GetMouseButtonDown(0)){
-
+			PlayerFire(0);
 		}
 		if (Input.GetMouseButtonDown(1)){
-
+			PlayerFire(1);
 		}
 		thisRigidbody.MovePosition(transform.position + transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
 		thisRigidbody.MovePosition(transform.position + transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
@@ -31,9 +32,21 @@ public class PlayerScript : MonoBehaviour {
 			thisRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 		}
 
+		if (Input.GetButtonDown("Interact")){
+
+		}
+
+		if (Input.GetButtonDown ("Console")){
+
+		}
+
 	}
 
-	public void PlayerFire(){
-
+	public void PlayerFire(int i){
+		if (i == 0){
+			//Fire 1
+		} else if (i == 1){
+			//Fire 2
+		}
 	}
 }
